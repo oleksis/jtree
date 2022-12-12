7099a493dfbdb750740128e7f97f956865a1c1ac
Oleksis Fraga Menéndez <oleksis.fraga@gmail.com>
2022-12-11 21:52:04 -0500

    bump version 0.1.1.post1 -> 0.1.2

--- jtree/__init__.py
+++ jtree/__init__.py
@@ -6,7 +6,7 @@ from textual.app import App, ComposeResult
 from textual.binding import Binding
 from textual.widgets import Tree, TreeNode
 
-__version__ = "0.1.1.post1"
+__version__ = "0.1.2"
 highlighter = ReprHighlighter()
 
 
