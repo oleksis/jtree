ffc38c80a377a81b20b496b4a3fe803a06b48538
Oleksis Fraga Menéndez <oleksis.fraga@gmail.com>
2022-12-18 18:55:52 -0500

    Add option for DebugPy logging

--- jtree/__main__.py
+++ jtree/__main__.py
@@ -2,12 +2,14 @@ from __future__ import annotations
 
 import argparse
 import json
+import logging
 import platform
 import sys
 
 from jtree import JSONTreeApp
 
 WINDOWS = platform.system() == "Windows"
+DEBUGPY_PORT = 5678
 
 
 def main():
@@ -20,9 +22,22 @@ def main():
         help="path to file, or stdin",
         default=sys.stdin,
     )
+    parser.add_argument(
+        "--log", nargs="?", help="Log level for enable debugpy", default="INFO"
+    )
 
     args = parser.parse_args()
     json_data = None
+    numeric_level = getattr(logging, args.log.upper(), None)
+
+    if not isinstance(numeric_level, int):
+        print(f"Invalid log level {args.log!r}")
+
+    if numeric_level == logging.DEBUG:
+        import debugpy
+
+        debugpy.listen(DEBUGPY_PORT)
+        debugpy.wait_for_client()
 
     with args.path as infile:
         try:
