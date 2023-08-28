7bb680efb98f42f26d328d9e0225e6cf15003f95
Oleksis Fraga Menéndez <oleksis.fraga@gmail.com>
2023-08-28 10:58:54 -0400

    Add clean_string_values from utils

---
+++ jtree/utils.py
@@ -0,0 +1,22 @@
+import contextlib
+import json
+
+
+def clean_string_values(obj):
+    if isinstance(obj, dict):
+        for key, value in obj.items():
+            if isinstance(value, str):
+                value = value.replace("\\r\\n", "")
+                with contextlib.suppress(json.JSONDecodeError):
+                    value = json.loads(value)
+
+            cleaned_value = clean_string_values(value)
+            obj[key] = cleaned_value
+    elif isinstance(obj, list):
+        for i, value in enumerate(obj):
+            cleaned_value = clean_string_values(value)
+            obj[i] = cleaned_value
+    elif isinstance(obj, str):
+        obj = obj.replace("\\r\\n", "").replace('\\"', '"')
+
+    return obj
